3a83a717c548dd34c502d35400e55bcc252a1a4a
vick_glb <vick_glb@me.com>
2019-10-21 22:39:53 +0800

    fixing 8 20191021

--- 8/main.go
+++ 8/main.go
@@ -6,48 +6,53 @@ func longestPalindrome(s string) string {
 	if len(s) <= 1 {
 		return s
 	}
-	var tmpMax, maxLen, minLen2, start, end, minLen int
+	var tmpMax, maxLen, start, end, minLen int
 	for i := 0; i < len(s); i++ {
 		if i-0 > len(s)-i-1 {
 			minLen = len(s) - i - 1
 		} else {
 			minLen = i - 0
 		}
-		for j := i + 1; j < len(s); j++ {
-			if s[i] == s[j] {
-				t := 0
-				if j-0 > len(s)-j-1 {
-					minLen2 = len(s) - i - 1
-				} else {
-					minLen2 = j - 0
-				}
-				for n := j + 1; n < minLen2; n++ {
+		// for j := i + 1; j < len(s); j++ {
+		// 	if s[i] == s[j] {
+		// 		t := 0
+		// 		if j-0 > len(s)-j-1 {
+		// 			minLen2 = len(s) - i - 1
+		// 		} else {
+		// 			minLen2 = j - 0
+		// 		}
+		// 		for n := j + 1; n < minLen2; n++ {
 
-					if s[i-n] == s[j+n] {
-						t = n
-					} else {
-						break
-					}
-				}
+		// 			if s[i-n] == s[j+n] {
+		// 				t = n
+		// 			} else {
+		// 				break
+		// 			}
+		// 		}
 
-				tmpMax = 2*t + j - i
-				if tmpMax > maxLen {
-					fmt.Println("wwwwwtmpmax:", tmpMax)
-					start = i - t
-					end = j + t
-					fmt.Println("111end:", end, "start:", start)
-					maxLen = tmpMax
-				}
+		// 		tmpMax = 2*t + j - i
+		// 		if tmpMax > maxLen {
+		// 			fmt.Println("wwwwwtmpmax:", tmpMax)
+		// 			start = i - t
+		// 			end = j + t
+		// 			fmt.Println("111end:", end, "start:", start)
+		// 			maxLen = tmpMax
+		// 		}
 
-			} else {
+		// 	} else {
 
-				break
-			}
-		}
+		// 		break
+		// 	}
+		// }
 
-		for k := i; k <= minLen; k++ {
-			if s[i+k] == s[i-k] {
-				tmpMax = 2 * k
+		// if s[i]==s[i+1] and i+1<len(s){
+
+		// }
+
+		for k := 1; k <= minLen; k++ {
+
+			if s[i+k] == s[i-k] && i >= 2 {
+				tmpMax = 2*k - 1
 				if tmpMax > maxLen {
 					fmt.Println("tmpmax:", tmpMax, "i:", i, "k:", k, "max:", maxLen)
 					start = i - k
@@ -63,10 +68,10 @@ func longestPalindrome(s string) string {
 		}
 
 	}
-	return s[start:end]
+	return s[start : end+1]
 }
 func main() {
-	s := longestPalindrome("aaabaaaaaaaa")
+	s := longestPalindrome("aabaaaas")
 	fmt.Printf("type: %T,[%v]", s, s)
 }
 
